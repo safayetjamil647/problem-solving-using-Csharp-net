6a896f2159a8c92dd9b1667b852d4e0a683d5e92
safayetjamil647 <work.jamil4@gmail.com>
2021-02-23 15:20:34 +0600

    day1

---
+++ Decissions/Decissions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Decissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Big giveway!");
+            Console.Write("Choose a door :1,2 or 3");
+            string userValue = Console.ReadLine();
+            if (userValue == "1")
+            {
+                string message = "You won a new car";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "2")
+            {
+                string message = "You won a new boat";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3")
+            {
+                string message = "You won a new plane";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                string message = "sorryyyy it s empty";
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int x;
+            int y;
+            int sum;
+            x = 6;
+            y = 5;
+            sum = x + y;
+
+            Console.WriteLine(sum);
+            */
+            
+            Console.WriteLine("Enter your first name");
+           string myFirstname=Console.ReadLine();
+           string myLastname = Console.ReadLine();
+            Console.WriteLine("hello"+ myFirstname+" is "+myLastname);
+        }
+    }
+}
