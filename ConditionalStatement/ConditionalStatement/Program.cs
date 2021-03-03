77063fb280a1416ff1f0e6fa4c7409686f016ec6
safayetjamil647 <work.jamil4@gmail.com>
2021-03-03 21:18:23 +0600

    conditional statement

---
+++ ConditionalStatement/ConditionalStatement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConditionalStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int weight = 700;
+            //Console.WriteLine(weight<300);
+
+            //int a = 7;
+            //int b = 9;
+            //bool condition = (b > a)&& (a+b<a*b);
+            //Console.WriteLine(condition);
+
+            Console.WriteLine("Enter Two numbers");
+            int firstNumber=int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+        }
+    }
+}
